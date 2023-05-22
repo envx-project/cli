c0f7e084388369f1bf7cbd864e555e3c910b9f60
Alexander Ng <alexng353@gmail.com>
2023-05-22 13:11:48 -0700

    gpg

--- src/constants.rs
+++
@@ -1,4 +0,0 @@
-// constants
-
-pub(crate) const BASE_URL: &'static str = "https://api.env-store.com";
-pub(crate) const GPG_ERROR: &'static str = "Failed to spawn gpg - is it installed on your system?\nSee the docs at https://docs.env-store.com/gpg/ for more info";
