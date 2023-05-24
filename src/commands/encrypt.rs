41814a18139e49b727027f7ccdc83c4a45b749ed
Alexander Ng <alexng353@gmail.com>
2023-05-23 17:10:22 -0700

    armored u8 vec???

--- src/commands/encrypt.rs
+++ src/commands/encrypt.rs
@@ -1,21 +1,36 @@
 use super::*;
-use crate::utils::encryption::encrypt;
 
 /// Encrypt a string using GPG
 #[derive(Parser)]
 pub struct Args {
-    /// recipient's public key
-    recipient: String,
+    // /// recipient's public key
+    // recipient: String,
 
-    /// string to encrypt
-    text: String,
+    // /// string to encrypt
+    // text: String,
 }
 
 pub async fn command(args: Args, _json: bool) -> Result<()> {
-    match encrypt(args.recipient, args.text).await {
-        Ok(encrypted) => println!("{}", encrypted),
-        Err(error) => eprintln!("{}", error),
-    }
+    let fingerprint = "734956E603EC58A94843A27D42449196796A2EEB";
+    let passphrase = "ANg8VC112358";
+    let keypair = crate::utils::rpgp::read_vault(fingerprint, passphrase)?;
 
+    // encrypt some arbitrary data
+    let data = b"hello world";
+
+    let encrypted = crate::utils::rpgp::encrypt(
+        keypair,
+        "971DBD09E27B4D4C06393604EFB7449DD97A113C",
+        "cock and ball torture",
+    )?;
     Ok(())
 }
+
+// pub async fn command(args: Args, _json: bool) -> Result<()> {
+//     match encrypt(args.recipient, args.text).await {
+//         Ok(encrypted) => println!("{}", encrypted),
+//         Err(error) => eprintln!("{}", error),
+//     }
+
+//     Ok(())
+// }
--- src/utils/rpgp.rs
+++ src/utils/rpgp.rs
@@ -1,10 +1,18 @@
-use anyhow::Context;
+use anyhow::{Context, Ok};
+use base64::Engine;
+use pgp::composed::PublicOrSecret;
 use pgp::composed::{key::SecretKeyParamsBuilder, KeyType};
 use pgp::crypto::{hash::HashAlgorithm, sym::SymmetricKeyAlgorithm};
-use pgp::types::{CompressionAlgorithm, KeyTrait, SecretKeyTrait};
-use pgp::{SignedPublicKey, SignedSecretKey};
+use pgp::types::{CompressionAlgorithm, KeyTrait, PublicKeyTrait, SecretKeyTrait};
+use pgp::{SecretKey, SignedPublicKey, SignedSecretKey};
 use smallvec::*;
 use std::fmt::Display;
+use std::io::{Cursor, Read};
+
+use rand::CryptoRng;
+use rand::Rng;
+use rand::SeedableRng;
+use rand_chacha::ChaCha20Rng;
 
 #[derive(Debug, Clone)]
 pub(crate) struct SignedRsaKeyPair {
@@ -35,6 +43,16 @@ impl Display for SignedRsaKeyPair {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         write!(f, "{}", self.public_key.to_armored_string(None).unwrap())
     }
+}
+
+pub(crate) fn get_vault_location() -> anyhow::Result<std::path::PathBuf, anyhow::Error> {
+    let path = home::home_dir()
+        .context("Failed to get home directory")?
+        .join(".config")
+        .join("envcli")
+        .join("keys");
+
+    Ok(path)
 }
 
 pub(crate) fn rsa_gen_key(
@@ -81,4 +99,79 @@ pub(crate) fn rsa_gen_key(
         secret_key: signed_secret_key,
         public_key: signed_public_key,
     })
+}
+
+pub(crate) fn read_vault(
+    fingerprint: &str,
+    password: &str,
+) -> anyhow::Result<SignedRsaKeyPair, anyhow::Error> {
+    let vault_location = get_vault_location()?.join(fingerprint.to_uppercase());
+    let priv_key = std::fs::read(vault_location.clone().join("priv.key"))?;
+
+    let cursor_wrapped_priv_key = Cursor::new(priv_key);
+
+    // make a keypair from the private key
+    let mut dearmored = pgp::composed::from_armor_many(cursor_wrapped_priv_key)?;
+
+    let unknown_key = dearmored.0.next().context("Failed to get key")??;
+    let passwd_fn = || password.to_string();
+
+    let signed_secret_key = unknown_key.into_secret();
+    let signed_public_key = signed_secret_key
+        .public_key()
+        .sign(&signed_secret_key, passwd_fn)
+        .context("Failed to sign public key")?;
+
+    let keypair = SignedRsaKeyPair {
+        secret_key: signed_secret_key,
+        public_key: signed_public_key,
+    };
+
+    anyhow::Ok(keypair)
+}
+
+pub(crate) fn read_pub_key(fingerprint: &str) -> anyhow::Result<String, anyhow::Error> {
+    Ok("".to_string())
+}
+
+pub(crate) fn encrypt(
+    keypair: SignedRsaKeyPair,
+    fingerprint: &str,
+    message: &str,
+) -> anyhow::Result<String, anyhow::Error> {
+    let vault_location = get_vault_location()?.join(fingerprint.to_uppercase());
+    let pub_key = std::fs::read(vault_location.clone().join("pub.key"))?;
+
+    let cursor_wrapped_pub_key = Cursor::new(pub_key);
+
+    // make a keypair from the public key
+    let mut dearmored = pgp::composed::from_armor_many(cursor_wrapped_pub_key)?;
+
+    let unknown_key = dearmored.0.next().context("Failed to get key")??;
+    let public_key = unknown_key.into_public();
+
+    let mut rng = ChaCha20Rng::from_entropy();
+
+    let encrypted_message = public_key.encrypt(&mut rng, message.as_bytes())?;
+
+    // let encrypted_message = encrypted_message
+
+    // .first()
+    // .iter()
+    // .map(|x| {
+    //     let y = x.as_bytes();
+    //     // println!("{:?}", y);
+    //     y.to_owned()
+    // })
+    // .collect::<Vec<_>>();
+
+    let utf8 = base64::engine::general_purpose::STANDARD.encode(encrypted_message.first().unwrap());
+
+    println!("{}", utf8);
+
+    // println!("{:?}", encrypted_message.clone());
+
+    // pgp::crypto::rsa::encrypt(rng, n, e, plaintext)
+
+    Ok("".to_string())
 }
