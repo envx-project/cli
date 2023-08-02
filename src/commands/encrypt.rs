e40a820c0d5cc46e619764158944e63aec707ffa
Alexander Ng <26424336+alexng353@users.noreply.github.com>
2023-08-02 12:35:38 +0700

    Update encrypt.rs

--- src/commands/encrypt.rs
+++ src/commands/encrypt.rs
@@ -12,7 +12,7 @@ pub struct Args {
 
 pub async fn command(args: Args, _json: bool) -> Result<()> {
     let fingerprint = "734956E603EC58A94843A27D42449196796A2EEB";
-    let passphrase = "ANg8VC112358";
+    let passphrase = "joever";
     let keypair = crate::utils::rpgp::read_vault(fingerprint, passphrase)?;
 
     // encrypt some arbitrary data
