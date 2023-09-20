95f533ffac29835c890764f247fc003b10280c08
Alexander Ng <alexng353@gmail.com>
2023-09-20 00:21:44 -0700

    feat: sdk upgrades & secret setting + signing

--- src/commands/set.rs
+++ src/commands/set.rs
@@ -1,49 +1,77 @@
 use super::*;
-use crate::utils::prompt::prompt_text;
-use anyhow::Ok;
+use crate::{
+    sdk::SetEnvParams,
+    utils::{
+        config::get_config,
+        prompt::prompt_text,
+        rpgp::{encrypt, get_primary_key},
+    },
+};
+use anyhow::{Context, Ok};
+use serde_json::json;
 
 /// SET an environment variable with a key=value pair
 /// also supports interactive mode
 #[derive(Parser)]
 pub struct Args {
+    #[clap(short, long)]
+    fingerprint: Option<String>,
+
     #[clap(trailing_var_arg = true)]
     kvpairs: Vec<String>,
 }
 
 pub async fn command(args: Args, _json: bool) -> Result<()> {
-    // if args.kvpairs.len() >= 1 {
-    //     for arg in args.kvpairs.clone() {
-    //         let split = &arg.splitn(2, "=").collect::<Vec<&str>>();
-    //         if split.len() != 2 {
-    //             eprintln!("Error: Invalid key=value pair");
-    //             std::process::exit(1);
-    //         }
+    let config = get_config()?;
+    let pubkey = get_primary_key()
+        .context("Failed to get primary key, try generating a new one with `envcli gen`")?;
+    if args.kvpairs.len() >= 1 {
+        for arg in args.kvpairs.clone() {
+            let split = &arg.splitn(2, "=").collect::<Vec<&str>>();
+            if split.len() != 2 {
+                eprintln!("Error: Invalid key=value pair");
+                std::process::exit(1);
+            }
 
-    //         let key = split[0].to_uppercase().to_string();
-    //         let value = split[1].to_string();
+            let key = split[0].to_uppercase().to_string();
+            let value = split[1].to_string();
 
-    //         println!("Setting {}={}", key, value);
-    //         crate::sdk::Client::set_env(key, value).await?;
-    //     }
+            println!("Setting {}={}", key, value);
 
-    //     return Ok(());
-    // }
+            let body = json!({
+                "key": key,
+                "value": value
+            });
 
-    // let key = match prompt_text("key") {
-    //     Good(key) => key.to_uppercase(),
-    //     Err(_) => {
-    //         return Err(anyhow::Error::msg("Error: Could not read key"));
-    //     }
-    // };
+            let encrypted = encrypt(body.to_string().as_str(), &pubkey)?;
 
-    // let value = match prompt_text("value") {
-    //     Good(value) => value,
-    //     Err(_) => {
-    //         return Err(anyhow::Error::msg("Error: Could not read value"));
-    //     }
-    // };
+            let body = SetEnvParams {
+                message: encrypted,
+                allowed_keys: vec![config.primary_key.clone()],
+                project_id: None,
+            };
 
-    // crate::sdk::Client::set_env(key, value).await?;
+            crate::sdk::SDK::set_env(body).await?;
+        }
+
+        return Ok(());
+    }
+
+    let key = match prompt_text("key") {
+        Good(key) => key.to_uppercase(),
+        Err(_) => {
+            return Err(anyhow::Error::msg("Error: Could not read key"));
+        }
+    };
+
+    let value = match prompt_text("value") {
+        Good(value) => value,
+        Err(_) => {
+            return Err(anyhow::Error::msg("Error: Could not read value"));
+        }
+    };
+
+    // crate::sdk::SDK::set_env(key, value).await?;
 
     Ok(())
 }
--- src/sdk.rs
+++ src/sdk.rs
@@ -1,5 +1,6 @@
 use anyhow::Ok;
 use serde::{Deserialize, Serialize};
+use serde_json::json;
 
 use super::*;
 
@@ -9,6 +10,13 @@ pub struct NewUserParams {
     pub user_id: String,
     pub pubkey: String,
     pub pubkey_hash: String,
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct SetEnvParams {
+    pub message: String,
+    pub allowed_keys: Vec<String>,
+    pub project_id: Option<String>,
 }
 
 const API_URL: &str = "http://localhost:3000";
@@ -31,6 +39,29 @@ impl SDK {
         } else {
             Err(anyhow::Error::msg(format!(
                 "Failed to create user: {}",
+                res.text().await?
+            )))
+        }
+    }
+
+    pub async fn set_env(body: SetEnvParams) -> Result<()> {
+        let client = reqwest::Client::new();
+
+        let res = client
+            .post(&format!("{}/secrets/new", API_URL))
+            .json(&body)
+            .send()
+            .await?;
+
+        let status = res.status();
+
+        dbg!(status);
+
+        if status.is_success() {
+            Ok(())
+        } else {
+            Err(anyhow::Error::msg(format!(
+                "Failed to set new secret: {}",
                 res.text().await?
             )))
         }
