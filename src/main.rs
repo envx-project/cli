ff66b8c16eb1f4611192239d58f5b261dd42b53c
Alexander Ng <alexng353@gmail.com>
2023-10-08 16:44:11 -0700

    settings command

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,6 @@ use clap::{Parser, Subcommand};
 use commands::*;
 
 mod commands;
-mod consts;
 mod errors;
 mod sdk;
 mod types;
@@ -27,7 +26,7 @@ pub struct Args {
 
 // Generates the commands based on the modules in the commands directory
 // Specify the modules you want to include in the commands_enum! macro
-commands_enum!(variables, set, unset, shell, run, encrypt, decrypt, gen, delete_key);
+commands_enum!(variables, set, unset, shell, run, encrypt, decrypt, gen, delete_key, settings);
 
 #[tokio::main]
 async fn main() -> Result<()> {
