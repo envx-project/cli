21c8e6bd21122e936863052bf55f0debf308b120
Alexander Ng <alexng353@gmail.com>
2023-12-28 23:56:16 -0800

    add project

---
+++ src/commands/project.rs
@@ -0,0 +1,21 @@
+use super::*;
+use crate::{sdk::SDK, utils::config::get_config};
+
+/// Create a new project
+#[derive(Parser)]
+pub struct Args {
+    /// Key
+    #[clap(short, long)]
+    key: Option<String>,
+}
+
+pub async fn command(args: Args, _json: bool) -> Result<()> {
+    let config = get_config()?;
+    let key = config.get_key_or_default(args.key)?;
+
+    let new_project_id = SDK::new_project(&key.fingerprint).await?;
+
+    println!("Created new project with ID: {}", new_project_id);
+
+    Ok(())
+}
