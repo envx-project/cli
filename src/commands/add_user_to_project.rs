a43c04df98e437c6a11e53024bc2315900f97908
Alexander Ng <alexng353@gmail.com>
2023-12-29 00:09:27 -0800

    push

--- src/commands/add_user_to_project.rs
+++ src/commands/add_user_to_project.rs
@@ -1,25 +1,24 @@
+use super::*;
 use crate::{
     sdk::{get_api_url, SDK},
     utils::{
-        auth::get_token, config::get_config, partial_variable::PartialVariable, rpgp::encrypt_multi,
+        auth::get_token, choice::Choice, config::get_config, partial_variable::PartialVariable,
+        rpgp::encrypt_multi,
     },
 };
 use reqwest::header;
 use serde_json::json;
-
-use super::*;
-use anyhow::Ok;
 
 /// Add a user to a project
 #[derive(Parser)]
 pub struct Args {
     /// Key to sign with
     #[clap(short, long)]
-    key: String,
+    key: Option<String>,
 
     /// Project ID to add user to
     #[clap(short, long)]
-    project_id: String,
+    project_id: Option<String>,
 
     /// User ID to add to project
     #[clap(short, long)]
@@ -29,14 +28,22 @@ pub struct Args {
 pub async fn command(args: Args, _json: bool) -> Result<()> {
     let config = get_config()?;
 
-    let key = config.get_key(&args.key)?;
+    let key = config.get_key_or_default(args.key)?;
     let uuid = key.uuid.clone().unwrap();
 
     let (_, user_public_key_to_add) = SDK::get_user(&key.fingerprint, &uuid, &args.user_id).await?;
 
-    let project_info = SDK::get_project_info(&args.project_id, &key.fingerprint, &uuid).await?;
+    let project_id = match args.project_id {
+        Some(p) => p,
+        None => match config.get_project() {
+            Ok(p) => p.project_id.clone(),
+            Err(_) => Choice::choose_project(&key.fingerprint).await?,
+        },
+    };
 
-    let (kvpairs, partials) = SDK::get_variables(&args.project_id, &key.fingerprint, &uuid).await?;
+    let project_info = SDK::get_project_info(&project_id, &key.fingerprint, &uuid).await?;
+
+    let (kvpairs, partials) = SDK::get_variables(&project_id, &key.fingerprint, &uuid).await?;
 
     let mut recipients = project_info
         .users
@@ -83,7 +90,7 @@ pub async fn command(args: Args, _json: bool) -> Result<()> {
     println!("Updated {} variables", res.len());
     println!("IDs: {:?}", res);
 
-    SDK::add_user_to_project(&key.fingerprint, &uuid, &args.user_id, &args.project_id).await?;
+    SDK::add_user_to_project(&key.fingerprint, &uuid, &args.user_id, &project_id).await?;
 
     Ok(())
 }
