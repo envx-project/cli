9f578d3c5b306003d4b7bc886249107743f7be66
Alexander Ng <alexng353@gmail.com>
2024-09-03 00:16:01 -0700

    fix: unset defaults to current project
    feat: add all flag to unset

--- src/commands/unset.rs
+++ src/commands/unset.rs
@@ -1,31 +1,50 @@
 use super::*;
+use crate::utils::choice::Choice;
+use crate::utils::partial_variable::ToParsed;
 use crate::utils::prompt;
 use crate::{sdk::SDK, utils::config::get_config};
-use anyhow::Context;
 
 /// Unset (delete) an environment variable
 #[derive(Parser)]
 pub struct Args {
-    /// Variable to delete
+    /// Variable to unset 
     #[clap(short, long)]
     variable: Option<String>,
 
     /// Key to use
     #[clap(short, long)]
     key: Option<String>,
+
+    #[clap(short, long)]
+    project_id: Option<String>,
+
+    #[clap(short, long, default_value_t = false)]
+    all: bool,
 }
 
 pub async fn command(args: Args) -> Result<()> {
-    let config = get_config().context("Failed to get config")?;
+    let config = get_config()?;
     let key = config.get_key_or_default(args.key)?;
+    let project_id = match args.all {
+        true => None,
+        false => {
+            Some(Choice::try_project(args.project_id, &key.fingerprint).await?)
+        }
+    };
 
     let variable = match args.variable {
         Some(v) => v,
         None => {
-            let (_, all_variables) =
-                SDK::get_all_variables(&key.fingerprint).await?;
-            prompt::prompt_options("Select variables to delete", all_variables)?
-                .id
+            let variables = if let Some(project_id) = project_id {
+                SDK::get_variables(&project_id, &key.fingerprint)
+                    .await?
+                    .1
+                    .to_parsed()
+            } else {
+                SDK::get_all_variables(&key.fingerprint).await?.1
+            };
+
+            prompt::prompt_options("Select variables to delete", variables)?.id
         }
     };
 
