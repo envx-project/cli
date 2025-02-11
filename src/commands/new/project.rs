c97e0e554a04c51a3217c7ea2f7fbbe32a53fbd7
flatypus [mac] <yhc3141@gmail.com>
2025-02-11 02:46:57 -0800

    feat: cli calls v2 functions to get and declare new project name

--- src/commands/new/project.rs
+++ src/commands/new/project.rs
@@ -1,5 +1,6 @@
 use super::*;
 use crate::{sdk::SDK, utils::config::get_config};
+use crate::utils::prompt::{prompt_text};
 
 /// Create a new project
 #[derive(Parser)]
@@ -7,12 +8,21 @@ pub struct Args {
     /// Key
     #[clap(short, long)]
     key: Option<String>,
+
+    /// Project name
+    #[clap(short, long)]
+    name: Option<String>,
 }
 
 pub async fn command(args: Args) -> Result<()> {
     let config = get_config()?;
     let key = config.get_key_or_default(args.key)?;
-    let new_project_id = SDK::new_project(&key.fingerprint).await?;
+
+    let name = args
+        .name
+        .unwrap_or_else(|| prompt_text("What is the name of this project?").unwrap());
+
+    let new_project_id = SDK::new_project(&key.fingerprint, &name).await?;
     println!("Created new project with ID: {}", new_project_id);
     Ok(())
 }
