8c307fe19166d7130b5231ae42add2295c0ab575
Alexander Ng <alexng353@gmail.com>
2025-05-25 15:33:04 -0700

    feat: better output for whoami; json output for whoami

--- src/commands/whoami.rs
+++ src/commands/whoami.rs
@@ -3,15 +3,23 @@ use crate::utils::config::Config;
 
 /// Print the primary key fingerprint and uuid
 #[derive(Parser)]
-pub struct Args {}
+pub struct Args {
+    #[clap(long)]
+    json: bool,
+}
 
-pub async fn command(_args: Args) -> Result<()> {
+pub async fn command(args: Args) -> Result<()> {
     let config = Config::get().context("Failed to get config")?;
     let primary_key = config.primary_key()?;
-    println!(
-        "{} - {}",
-        &primary_key.fingerprint[..8],
-        primary_key.uuid.unwrap_or("Not on remote".into())
-    );
+    if args.json {
+        println!("{}", serde_json::to_string(&primary_key)?);
+        return Ok(());
+    } else {
+        println!("fingerprint: {}", &primary_key.fingerprint[..8]);
+        println!(
+            "uuid: {}",
+            primary_key.uuid.unwrap_or("Not on remote".into())
+        );
+    }
     Ok(())
 }
