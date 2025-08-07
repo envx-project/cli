d291c74a64aba14c906f9b9b32ec70292a4264d9
Alexander Ng <alexng353@gmail.com>
2025-08-07 07:31:35 -0700

    chore: remove unused code

--- src/sdk.rs
+++ src/sdk.rs
@@ -304,37 +304,6 @@ impl SDK {
         Ok(kvpairs)
     }
 
-    pub async fn remove_users_from_project(
-        key: &UnlockedKey,
-        users_to_remove: &Vec<String>,
-        project_id: &str,
-    ) -> Result<()> {
-        // url: /project/:id/remove-user
-        let client = reqwest::Client::new();
-
-        let body = json!({
-            "users": users_to_remove
-        });
-
-        let url =
-            api_url().join(&format!("/project/{}/remove-user", project_id))?;
-
-        let res = client
-            .post(url)
-            .header(header::AUTHORIZATION, key.auth_token()?.bearer())
-            .json(&body)
-            .send()
-            .await?;
-
-        let status = res.status();
-
-        if status.is_success() {
-            Ok(())
-        } else {
-            bail!("Failed to remove user from project: {}", res.text().await?)
-        }
-    }
-
     pub async fn delete_project(
         key: &UnlockedKey,
         project_id: &str,
